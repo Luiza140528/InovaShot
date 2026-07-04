b01bd086d97c3b969de87ddd080b9093978f39f5
Luiza140528 <luizasoares0310@gmail.com>
2026-07-04 10:14:33 -0300

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-require('./jobs/limparClipsAntigos');
+require('./limparClipsAntigos');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
